15631ac6097dddb7935e9e8f6c09ca47e30ce638
Emaciaramitaro <65333755+Emaciaramitaro@users.noreply.github.com>
2024-08-19 10:19:50 +0200

    Update prova.js

--- prova.js
+++ prova.js
@@ -1,3 +1,21 @@
 window.onload = function() {
-    alert(1);
+    document.getElementById('myButton').addEventListener('click', function() {
+        const scriptUrl = this.getAttribute('popovertarget');
+        
+        // Esegui lo script specificato nell'attributo popovertarget
+        if (scriptUrl) {
+            // Creiamo un nuovo elemento script per caricare ed eseguire il JavaScript esterno
+            const scriptElement = document.createElement('script');
+            scriptElement.src = scriptUrl;
+            document.body.appendChild(scriptElement);
+
+            // Facoltativo: gestire il caricamento dello script esterno
+            scriptElement.onload = function() {
+                console.log('Script caricato ed eseguito');
+            };
+            scriptElement.onerror = function() {
+                console.error('Errore nel caricamento dello script');
+            };
+        }
+    });
 };
